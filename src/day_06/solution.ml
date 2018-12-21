334d98fa7a7f1d06587cda1438121f15336c1b7e
Lukas Kollmer <lukas.kollmer@gmail.com>
2018-12-21 16:22:48 +0100

    [day 6] part 1

---
+++ src/day_06/solution.ml
@@ -0,0 +1,75 @@
+let read_lines filename =
+  let file = open_in filename in
+  let rec imp acc = 
+    try imp (input_line file :: acc)
+    with End_of_file -> close_in file; List.rev acc
+  in
+  imp []
+
+
+
+let point_of_string str =
+  let split = String.split_on_char ',' str in
+  let x = int_of_string (List.hd split) in
+  let y = int_of_string (split |> List.tl |> List.hd |> String.trim) in
+  (x, y)
+
+let string_of_point (id, x, y) = Printf.sprintf "([%i] %i, %i)" id x y
+let print_point p = p |> string_of_point |> print_endline
+
+
+
+let rangei a b =
+  let rec imp l i =
+    if i < a then l else imp (i::l) (i-1)
+  in
+  imp [] b
+
+let range a b = rangei a (b-1)
+
+
+let count_where f l =
+  let rec imp acc = function
+    | [] -> acc
+    | x::xs -> if f x then imp (acc+1) xs else imp acc xs
+  in
+  imp 0 l
+
+let () =
+  let input = read_lines "input.txt" in
+  let points = List.mapi (fun i s -> let (x, y) = point_of_string s in (i, x, y)) input in
+
+  (* part 1 *)
+
+  let (width, height) = points |> List.fold_left (fun (w, h) (_, x, y) -> max w x, max h y) (0, 0) in
+  let (width, height) = (width + 1, height + 1) in
+
+
+  let d (x, y) (x', y') = (abs (x - x')) + (abs (y - y')) in
+
+  let distances = Array.init height (fun _ -> Array.make width (-1, -1)) in
+
+  range 0 width |> List.iter (fun x -> range 0 height |> List.iter (fun y ->
+    let nearest_points = points |> List.map (fun (id, x', y') -> id, d (x, y) (x', y')) |> List.sort (fun (_, d) (_, d') -> d - d') in
+    distances.(y).(x) <- match nearest_points with
+    | (id, distance) :: (id', distance') :: _ -> if distance = distance' then (-1, -1) else (id, distance)
+    | _ -> failwith "should never reach here"
+  )) ;
+
+
+  let points_with_finite_fields = points |> List.filter (fun (id, x, y) ->
+    let exists_with f =
+      points |> List.exists (fun (id', x', y') -> id <> id' && f (x', y'))
+    in
+    exists_with (fun (x', y') -> x' < x && y' < y) &&
+    exists_with (fun (x', y') -> x' < x && y' > y) &&
+    exists_with (fun (x', y') -> x' > x && y' < y) &&
+    exists_with (fun (x', y') -> x' > x && y' > y)
+  ) in
+
+  let distances_as_list = distances |> Array.map Array.to_list |> Array.to_list |> List.flatten in
+
+  points_with_finite_fields
+  |> List.map (fun (id, _, _) -> id, distances_as_list |> count_where (fun (id', _) -> id = id'))
+  |> List.sort (fun (_, count) (_, count') -> count' - count)
+  |> List.hd |> (fun (id, size) -> Printf.printf "[part 1] id: %i size: %i" id size) ;
