3f85717ecf3349c0298eb0fe16c45c00a5065c41
Lukas Kollmer <lukas.kollmer@gmail.com>
2019-12-03 17:56:44 +0100

    2019/03 pt2

--- src/2019/day_03/solution.ml
+++ src/2019/day_03/solution.ml
@@ -21,17 +21,19 @@ let cmp (x: int) (y: int): cmp =
 
 (* A Binary Tree data structure storing coordinates in a two-dimensional plane *)
 module Tree = struct
-  type t = int * int
+  type t = int * int * int
   type tree = Leaf | Node of t * tree * tree
 
   let hash = Hashtbl.hash
 
-  let compare_hash x y =
-    cmp (hash x) (hash y)
+  let compare_hash (x, y, _) (x', y', _) =
+    cmp (hash (x, y)) (hash (x', y'))
 
-  let compare (x, y as p) (x', y' as p') =
+  (* we can safely ignore the distance since we're interested in the shortest path, meaning that
+  if the tree simply does not override existing points, it'll always store the smallest distance *)
+  let compare (x, y, _) (x', y', _) =
     if x = x' && y = y' then Eq
-    else cmp (manhattan p) (manhattan p')
+    else cmp (manhattan (x, y)) (manhattan (x', y'))
 
   let rec insert v = function
     | Leaf -> Node (v, Leaf, Leaf)
@@ -53,6 +55,14 @@ module Tree = struct
         | Gt -> contains v r)
       | Lt -> contains v l
       | Gt -> contains v r
+  
+  let rec first_where f = function
+    | Leaf -> None
+    | Node (v, l, r) ->
+      match f v with
+      | Eq -> Some v
+      | Lt -> first_where f l
+      | Gt -> first_where f r
   
   let rec to_list = function
     | Leaf -> []
@@ -79,12 +89,13 @@ let get_move s =
 
 (* Returns the list of positions of the wire, relative to an origin (0, 0) in the bottom left corner *)
 let parse_wire w =
-  let rec walk ((x, y) as pos) path = function
+  let rec walk ((x, y, d) as pos) path = function
     | [] -> path
     | (dir, 0)::xs -> walk pos path xs
     | (dir, n)::xs ->
       let imp f =
-        let npos = f pos in
+        let (x, y) = f (x, y) in
+        let npos = (x, y, d+1) in
         walk npos (Tree.insert npos path) ((dir, n-1)::xs)
       in
       match dir with
@@ -93,7 +104,7 @@ let parse_wire w =
       | Left  -> imp (fun (x, y) -> x-1, y)
       | Right -> imp (fun (x, y) -> x+1, y)
   in
-  walk (0,0) Tree.Leaf (List.map get_move w)
+  walk (0, 0, 0) Tree.Leaf (List.map get_move w)
 
 
 
@@ -103,13 +114,23 @@ let () =
   let w1 = parse_wire (List.nth input 0) in
   let w2 = parse_wire (List.nth input 1) in
 
+  let intersections = w1 |> Tree.to_list |> List.filter (fun p -> Tree.contains p w2) in
+
   (* Part 1 *)
-  w1
-  |> Tree.to_list
-  |> List.filter (fun pos -> Tree.contains pos w2)
-  |> List.map manhattan
+  intersections
+  |> List.map (fun (x, y, _) -> manhattan (x, y))
   |> List.sort Int.compare
   |> List.hd
   |> Printf.printf "Part1: %i\n" ;
+
+  (* Part 2 *)
+  intersections
+  |> List.filter_map (fun ((x, y, d) as p) ->
+    match Tree.first_where (fun ((x', y', d') as p') -> if x = x' && y = y' then Eq else Tree.compare_hash p p') w2 with
+    | None -> None
+    | Some (x', y', d') -> Some (d + d') )
+  |> List.sort Int.compare
+  |> List.hd
+  |> Printf.printf "Part2: %i\n" ;
 
   ()
