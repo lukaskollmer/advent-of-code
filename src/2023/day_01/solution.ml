51e597ab7f8f5426eb13af82348e6476d649f9e9
Lukas Kollmer <hey@lukaskollmer.de>
2023-12-01 12:07:06 +0100

    2023/01 pt02

--- src/2023/day_01/solution.ml
+++ src/2023/day_01/solution.ml
@@ -30,7 +30,6 @@ let get_chars s =
 
 let pt01 input =
   input
-    |> List.map get_chars
     |> List.map (fun x -> (first_where is_numeric_letter x, last_where is_numeric_letter x))
     |> List.map (fun (x, y) -> (Option.get x, Option.get y))
     |> List.map (fun (x, y) -> (int_of_char' x) * 10 + (int_of_char' y))
@@ -40,7 +39,60 @@ let pt01 input =
 
 
 
+
+
+
+
+let rec list_prefix n = function
+  | [] -> []
+  | x::xs -> if n = 0 then [] else x::(list_prefix (n-1) xs)
+;;
+
+
+
+let digits_mapping = [
+  (1, "one");
+  (2, "two");
+  (3, "three");
+  (4, "four");
+  (5, "five");
+  (6, "six");
+  (7, "seven");
+  (8, "eight");
+  (9, "nine")
+] |> List.map (fun (x, y) -> (x, get_chars y))
+;;
+
+
+let leading_number mapping l = 
+  mapping
+  |> first_where (fun (x, s) -> list_prefix (List.length s) l = s)
+  |> Option.map fst
+;;
+
+let rec first_number' mapping (x::xs) =
+  if is_numeric_letter x then int_of_char' x
+  else match leading_number mapping (x::xs) with
+    | Some i -> i
+    | None -> first_number' mapping xs
+;;
+
+let first_number x = first_number' digits_mapping x ;;
+
+let last_number x = first_number' (List.map (fun (a, b) -> (a, List.rev b)) digits_mapping) (List.rev x) ;;
+
+
+let pt02 input =
+  input
+    |> List.map (fun x -> (first_number x, last_number x))
+    |> List.map (fun (a, b) -> a * 10 + b)
+    |> List.fold_left (+) 0
+;;
+
+
+
 let () =
-  let input = read_lines "input.txt" in
-  input |> pt01 |> Printf.printf "Pt01 sol: %i\n"
+  let input = read_lines "input.txt" |> List.map get_chars in
+  input |> pt01 |> Printf.printf "Pt01 sol: %i\n" ;
+  input |> pt02 |> Printf.printf "Pt02 sol: %i\n" ;
 ;;
