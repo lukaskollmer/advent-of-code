c0c3dbe6b89f7b340a57a69c339b58eec39b68dd
Lukas Kollmer <hey@lukaskollmer.de>
2023-12-11 14:28:55 +0100

    2023/11 pt02 (make it even more beautiful)

--- src/2023/day_11/solution.ml
+++ src/2023/day_11/solution.ml
@@ -15,7 +15,6 @@ let read_lines filename =
   imp []
 ;;
 
-
 let get_chars s =
   let rec imp l = function
     | -1 -> l
@@ -24,15 +23,12 @@ let get_chars s =
   imp [] (String.length s - 1)
 ;;
 
-
-
 let flat_mapi f l =
   let rec imp i acc = function
     | [] -> acc
     | x::xs -> imp (i+1) (acc @ f i x) xs
   in imp 0 [] l
 ;;
-
 
 let mk_range_excl a b =
   let rec imp acc = function
@@ -43,29 +39,28 @@ let mk_range_excl a b =
 ;;
 
 
-
 (* Parse input; return list of galaxies, universe width, universe height *)
 let parse_input input =
-  let poss = input |> flat_mapi (fun y l -> l |> get_chars |> flat_mapi (fun x -> function '#' -> [(x,y)] | _ -> [])) in
-  (poss, input |> List.hd |> String.length, List.length input)
+  let univ = input |> flat_mapi (fun y l -> l |> get_chars |> flat_mapi (fun x -> function '#' -> [(x,y)] | _ -> [])) in
+  (univ, input |> List.hd |> String.length, List.length input)
 ;;
 
 
 let enlargen n w h univ =
-  let all_rows, all_cols = univ |> List.fold_left (fun (rows, cols) (x,y) -> (IntSet.add y rows, IntSet.add x cols)) (IntSet.empty, IntSet.empty) in
-  let empty_rows = all_rows |> IntSet.diff (mk_range_excl 0 h |> IntSet.of_list) in
-  let empty_cols = all_cols |> IntSet.diff (mk_range_excl 0 w |> IntSet.of_list) in
-  univ |> List.map (fun (x,y) ->
-    (
-      x + (max 1 (n-1)) * ((empty_cols |> IntSet.filter (fun x' -> x' < x) |> IntSet.cardinal)),
-      y + (max 1 (n-1)) * ((empty_rows |> IntSet.filter (fun y' -> y' < y) |> IntSet.cardinal))
-    )
-  )
+  let n = max 1 (n-1) in
+  let empty_rows, empty_cols = univ |> List.fold_left (fun (rows, cols) (x, y) ->
+    (IntSet.remove y rows, IntSet.remove x cols)
+  ) (mk_range_excl 0 h |> IntSet.of_list, mk_range_excl 0 w |> IntSet.of_list) in
+  let count_lt x s = s |> IntSet.filter ((>=) x) |> IntSet.cardinal in
+  univ |> List.map (fun (x,y) -> (
+    x + n * (count_lt x empty_cols),
+    y + n * (count_lt y empty_rows)
+  ))
 ;;
 
 
-let shortest_path (x_src, y_src) (x_dst, y_dst) =
-  abs (x_src - x_dst) + abs (y_src - y_dst)
+let shortest_path (x, y) (x', y') =
+  abs (x-x') + abs (y-y')
 ;;
 
 let run factor input =
@@ -78,7 +73,10 @@ let run factor input =
       else PosPairSet.add (g1,g2) pairs
     ) pairs
   ) PosPairSet.empty in
-  pairs |> PosPairSet.to_list |> List.map (fun (src, dst) -> shortest_path src dst) |> List.fold_left (+) 0
+  pairs
+    |> PosPairSet.to_list
+    |> List.map (fun (src, dst) -> shortest_path src dst)
+    |> List.fold_left (+) 0
 ;;
 
 
