91d1baad72cf90e880aa6c30627b858b1b671e30
Lukas Kollmer <hey@lukaskollmer.de>
2024-12-07 13:49:39 +0100

    2024/07 "it's faster now"

--- src/2024/day_07/solution.ml
+++ src/2024/day_07/solution.ml
@@ -1,3 +1,5 @@
+module IntSet = Set.Make(Int) ;;
+
 let read_lines filename =
   let file = open_in filename in
   let rec imp acc = 
@@ -45,16 +47,35 @@ let string_of_list l = l |> List.to_seq |> String.of_seq ;;
 
 let rec op_perms = function
   | 0 -> []
-  | 1 -> [[Add]; [Mul]]
-  | n -> (op_perms (n-1)) |> List.concat_map (fun l -> [Add::l; Mul::l])
+  | 1 -> [[Op Add]; [Op Mul]]
+  | n -> (op_perms (n-1)) |> List.concat_map (fun l -> [Op Add::l; Op Mul::l])
 ;;
 
 
 let rec op_perms' = function
   | 0 -> []
-  | 1 -> [[Add]; [Mul]; [Concat]]
-  | n -> (op_perms' (n-1)) |> List.concat_map (fun l -> [Add::l; Mul::l; Concat::l])
+  | 1 -> [[Op Add]; [Op Mul]; [Op Concat]]
+  | n -> (op_perms' (n-1)) |> List.concat_map (fun l -> [Op Add::l; Op Mul::l; Op Concat::l])
 ;;
+
+
+module IntHashTbl = Hashtbl.Make(Int) ;;
+
+
+let memo_rec f =
+  let h = Hashtbl.create 16 in
+  let rec g x =
+    try Hashtbl.find h x
+    with Not_found ->
+      let y = f g x in
+      Hashtbl.add h x y;
+      y
+  in
+  g
+;;
+
+(* let op_perms = memo_rec op_perms ;;
+let op_perms' = memo_rec op_perms' ;; *)
 
 
 let dump_eq eq =
@@ -81,19 +102,6 @@ let rec pow a = function
     b * b * (if n mod 2 = 0 then 1 else a)
 
 
-let solve eq =
-  let rec imp acc = function
-    | [] -> acc
-    | Op Add :: Val n :: eq -> imp (acc + n) eq
-    | Op Mul :: Val n :: eq -> imp (acc * n) eq
-    | Op Concat :: Val n :: eq -> imp (acc * (pow 10 (digits n)) + n) eq
-    | _ -> failwith "invalid eq format"
-  in
-  match eq with
-    | Val n :: eq -> imp n eq
-    | _ -> failwith "invalid eq format"
-;;
-
 
 let intersperse xs ys =
   let rec imp acc = function
@@ -105,21 +113,39 @@ let intersperse xs ys =
 ;;
 
 
+
+let solve res eq =
+  let rec imp acc eq =
+    if acc > res then false else match eq with
+    | [] -> acc = res
+    | Op Add :: Val n :: eq -> imp (acc + n) eq
+    | Op Mul :: Val n :: eq -> imp (acc * n) eq
+    | Op Concat :: Val n :: eq -> imp (acc * (pow 10 (digits n)) + n) eq
+    | _ -> failwith "invalid eq format"
+  in
+  match eq with
+    | Val n :: eq -> imp n eq
+    | _ -> failwith "invalid eq format"
+;;
+
+
 let pt01 eqs =
   eqs |> List.fold_left (fun acc (res,eq) -> 
     let ops_perms = op_perms (List.length eq - 1) in
-    let eqs = ops_perms |> List.map (fun ops -> ops |> List.map (fun op -> Op op)) |> List.map (intersperse eq) in
-    let has_solution = eqs |> List.exists (fun eq -> solve eq = res) in
+    let eqs = ops_perms |> List.map (intersperse eq) in
+    let has_solution = eqs |> List.exists (fun eq -> solve res eq) in
     acc + if has_solution then res else 0
   ) 0
 ;;
 
 
+
 let pt02 eqs =
-  eqs |> List.fold_left (fun acc (res,eq) -> 
-    let ops_perms = op_perms' (List.length eq - 1) in
-    let eqs = ops_perms |> List.map (fun ops -> ops |> List.map (fun op -> Op op)) |> List.map (intersperse eq) in
-    let has_solution = eqs |> List.exists (fun eq -> solve eq = res) in
+  let op_perms = Array.init (eqs |> List.fold_left (fun acc (_,eq) -> max acc (List.length eq + 1)) 0) op_perms' in
+  eqs |> List.fold_left (fun acc (res,eq) ->
+    let ops_perms = op_perms.(List.length eq - 1) in
+    let eqs = ops_perms |> List.map (intersperse eq) in
+    let has_solution = eqs |> List.exists (fun eq -> solve res eq) in
     acc + if has_solution then res else 0
   ) 0
 ;;
