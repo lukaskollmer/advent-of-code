ac11be70e69ea1f046ba9c8286025413adccc5c1
Lukas Kollmer <hey@lukaskollmer.de>
2024-12-08 12:34:55 +0100

    2024/08 pt02

--- src/2024/day_08/solution.ml
+++ src/2024/day_08/solution.ml
@@ -41,21 +41,6 @@ let parse lines =
 
 
 
-let get_antennas map =
-  let size = Array.length map in
-  Seq.unfold (fun (x,y) ->
-    if y >= size then None else
-    Some ((x, y, map.(y).(x)), (if x = size-1 then (0,y+1) else (x+1,y)))
-  ) (0,0) |> Seq.filter (function (_, _, Empty) -> false | (_, _, Antenna _) -> true)
-;;
-
-
-let comp_antinode_pos (x,y) (x',y') =
-  let diff = (x-x', y-y') in
-  (x+fst diff, y+snd diff)
-;;
-
-
 let dump_map map antinodes =
   let antinodes = List.of_seq antinodes in
   map |> Array.iteri (fun y row ->
@@ -68,24 +53,64 @@ let dump_map map antinodes =
 ;;
 
 
-let pt01 map =
+let valid_pos size (x,y) =
+  x >= 0 && y >= 0 && x < size && y < size
+;;
+
+
+let get_antennas map =
+  let size = Array.length map in
+  Seq.unfold (fun (x,y) ->
+    if y >= size then None else
+    Some ((x, y, map.(y).(x)), (if x = size-1 then (0,y+1) else (x+1,y)))
+  ) (0,0) |> Seq.filter (function (_, _, Empty) -> false | (_, _, Antenna _) -> true)
+;;
+
+
+
+let antinodes1 size p1 p2 =
+  let imp (x,y) (x',y') =
+    let (d,d') = (x-x', y-y') in
+    (x+d, y+d')
+  in
+  Seq.(cons (imp p1 p2) (cons (imp p2 p1) empty))
+;;
+
+
+let antinodes2 size p1 p2 =
+  let imp (x,y) (x',y') =
+    let (d,d') = (x-x', y-y') in
+    Seq.unfold (fun (x,y) ->
+      let next = (x+d,y+d') in
+      if valid_pos size next then (Some (next,next)) else None
+    ) (x,y) |> Seq.cons (x,y)
+  in
+  Seq.append (imp p1 p2) (imp p2 p1)
+;;
+
+
+
+
+let run antinodes map =
   let size = Array.length map in
   (* dump_map map Seq.empty ; *)
   let antennas = get_antennas map in
   let antinodes = Seq.map_product (fun (x,y,c) (x',y',c') ->
       if c <> c' || (x,y) = (x',y') then None else
-        Some [comp_antinode_pos (x,y) (x',y'); comp_antinode_pos (x',y') (x,y)]
+        Some (antinodes size (x,y) (x',y'))
     ) antennas antennas
-    |> Seq.filter_map (id)
-    |> Seq.flat_map (List.to_seq)
-    |> Seq.filter (fun (x,y) -> x >= 0 && y >= 0 && x < size && y < size)
+    |> Seq.filter_map id
+    |> Seq.flat_map id
+    |> Seq.filter (valid_pos size)
   in
   (* dump_map map antinodes ; *)
   antinodes |> PosSet.of_seq |> PosSet.cardinal
+;;
 
 
 let () =
   Printexc.record_backtrace true ;
   let input = read_lines "input.txt" in
   let map = input |> List.map get_chars |> parse in
-  map |> pt01 |> Printf.printf "pt01: %i\n%!" ;
+  map |> run antinodes1 |> Printf.printf "pt01: %i\n%!" ;
+  map |> run antinodes2 |> Printf.printf "pt01: %i\n%!" ;
